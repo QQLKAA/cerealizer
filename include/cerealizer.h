47c1344f7ced2741ae96b5098f2298606c992ecd
Bartłomiej Kudzia <bar.kud.11340@gmail.com>
2023-12-11 15:43:48 +0100

    Add serializing and deserializing uint32_t

--- include/cerealizer.h
+++ include/cerealizer.h
@@ -1,6 +1,9 @@
 #ifndef CEREALIZER_H
 #define CEREALIZER_H
 
+#include <cstdint>
+#include <vector>
+
 #ifndef CEREALIZER_LITTLE_ENDIAN
 #error "Cerealizer only supports little endian"
 #endif
@@ -8,7 +11,60 @@
 namespace Cerealizer
 {
 
+struct Context
+{
+    std::vector<uint8_t> buffer;
+    size_t cursor;
+};
 
+template<typename T>
+void Serialize(Context& context, const T& data)
+{
+    data.Serialize(context);
+}
+
+template<typename T>
+bool Deserialize(Context &context,  T& data)
+{
+    return data.Deserialize(context);
+}
+
+template<typename T>
+void SerializePrimitive(Context &context, const T& data)
+{
+    size_t spaceAvailable = context.buffer.size() - context.cursor;
+    if (spaceAvailable < sizeof(T))
+    {
+        context.buffer.resize(context.cursor + sizeof(T));
+    }
+
+    *reinterpret_cast<T*>(&context.buffer[context.cursor]) = data;
+}
+
+template<>
+void Serialize(Context &context, const uint32_t& data)
+{
+    SerializePrimitive(context, data);
+}
+
+template<typename T>
+bool DeserializePrimitive(Context &context, T& data)
+{
+    size_t spaceRemaining = context.buffer.size() - context.cursor; 
+    if (spaceRemaining < sizeof(T))
+    {
+        return false;
+    }
+
+    data = *reinterpret_cast<T*>(&context.buffer[context.cursor]);
+    return true;
+}
+
+template<>
+bool Deserialize(Context &context, uint32_t& data)
+{
+    return DeserializePrimitive(context, data);
+}
 
 }
 
