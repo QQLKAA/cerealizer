2cbe1ae3b28f09b03df88025f082eebad0b5c160
Bartłomiej Kudzia <bar.kud.11340@gmail.com>
2023-12-11 23:11:09 +0100

    Add tests for all primitives

--- tests/primitives_test.cpp
+++ tests/primitives_test.cpp
@@ -4,7 +4,31 @@
 
 using namespace Cerealizer;
 
-TEST(PrimitevesTest, HandlesUint32) 
+TEST(PrimitivesTest, HandlesUint8) 
+{
+    Context ctx;
+    uint8_t x = 0x12, y = 0;
+
+    Serialize(ctx, x);
+    ctx.cursor = 0;
+    Deserialize(ctx, y);
+
+    EXPECT_EQ(x, y);
+}
+
+TEST(PrimitivesTest, HandlesUint16) 
+{
+    Context ctx;
+    uint16_t x = 0x1234, y = 0;
+
+    Serialize(ctx, x);
+    ctx.cursor = 0;
+    Deserialize(ctx, y);
+
+    EXPECT_EQ(x, y);
+}
+
+TEST(PrimitivesTest, HandlesUint32) 
 {
     Context ctx;
     uint32_t x = 0x12345678, y = 0;
@@ -14,4 +38,101 @@ TEST(PrimitevesTest, HandlesUint32)
     Deserialize(ctx, y);
 
     EXPECT_EQ(x, y);
+}
+
+
+TEST(PrimitivesTest, HandlesUint64) 
+{
+    Context ctx;
+    uint64_t x = 0x1234567890123456, y = 0;
+
+    Serialize(ctx, x);
+    ctx.cursor = 0;
+    Deserialize(ctx, y);
+
+    EXPECT_EQ(x, y);
+}
+
+TEST(PrimitivesTest, HandlesInt8) 
+{
+    Context ctx;
+    uint8_t x = -125, y = 0;
+
+    Serialize(ctx, x);
+    ctx.cursor = 0;
+    Deserialize(ctx, y);
+
+    EXPECT_EQ(x, y);
+}
+
+TEST(PrimitivesTest, HandlesInt16) 
+{
+    Context ctx;
+    uint16_t x = -1024, y = 0;
+
+    Serialize(ctx, x);
+    ctx.cursor = 0;
+    Deserialize(ctx, y);
+
+    EXPECT_EQ(x, y);
+}
+
+TEST(PrimitivesTest, HandlesInt32) 
+{
+    Context ctx;
+    int32_t x = -1048576, y = 0;
+
+    Serialize(ctx, x);
+    ctx.cursor = 0;
+    Deserialize(ctx, y);
+
+    EXPECT_EQ(x, y);
+}
+
+TEST(PrimitivesTest, HandlesInt64) 
+{
+    Context ctx;
+    int64_t x = -1099511627776, y = 0;
+
+    Serialize(ctx, x);
+    ctx.cursor = 0;
+    Deserialize(ctx, y);
+
+    EXPECT_EQ(x, y);
+}
+
+TEST(PrimitivesTest, HandlesBoolean) 
+{
+    Context ctx;
+    bool x = true, y = 0;
+
+    Serialize(ctx, x);
+    ctx.cursor = 0;
+    Deserialize(ctx, y);
+
+    EXPECT_EQ(x, y);
+}
+
+TEST(PrimitivesTest, HandlesMultipleValues)
+{
+    Context ctx;
+
+    int32_t x1 = 1234567, y1; 
+    int8_t x2 = 110, y2; 
+    int16_t x3 = 12345, y3;
+
+    Serialize(ctx, x1);
+    Serialize(ctx, x2);
+    Serialize(ctx, x3);
+    ASSERT_EQ(ctx.cursor, sizeof(int32_t) + sizeof(int8_t) + sizeof(int16_t));
+
+    ctx.cursor = 0;
+    Deserialize(ctx, y1);
+    Deserialize(ctx, y2);
+    Deserialize(ctx, y3);
+    ASSERT_EQ(ctx.cursor, sizeof(int32_t) + sizeof(int8_t) + sizeof(int16_t));
+
+    ASSERT_EQ(x1, y1);
+    ASSERT_EQ(x2, y2);
+    ASSERT_EQ(x3, y3);
 }
