27af3ef616d972c343d1bdf73f97adafc0c273cc
Bartłomiej Kudzia <bar.kud.11340@gmail.com>
2023-12-14 11:33:06 +0100

    Add string support

--- include/cerealizer.h
+++ include/cerealizer.h
@@ -1,7 +1,10 @@
 #ifndef CEREALIZER_H
 #define CEREALIZER_H
 
+#include <cassert>
 #include <cstdint>
+#include <limits>
+#include <string>
 #include <vector>
 
 #ifndef CEREALIZER_LITTLE_ENDIAN
@@ -18,19 +21,19 @@ struct Context
 };
 
 template<typename T>
-void Serialize(Context& context, const T& data)
+inline void Serialize(Context& context, const T& data)
 {
     data.Serialize(context);
 }
 
 template<typename T>
-bool Deserialize(Context &context,  T& data)
+inline bool Deserialize(Context &context,  T& data)
 {
     return data.Deserialize(context);
 }
 
 template<typename T>
-void SerializePrimitive(Context &context, const T& data)
+inline void SerializePrimitive(Context &context, const T& data)
 {
     size_t spaceAvailable = context.buffer.size() - context.cursor;
     if (spaceAvailable < sizeof(T))
@@ -43,61 +46,80 @@ void SerializePrimitive(Context &context, const T& data)
 }
 
 template<>
-void Serialize(Context &context, const uint8_t& data)
+inline void Serialize(Context &context, const uint8_t& data)
 {
     SerializePrimitive(context, data);
 }
 
 template<>
-void Serialize(Context &context, const uint16_t& data)
+inline void Serialize(Context &context, const uint16_t& data)
 {
     SerializePrimitive(context, data);
 }
 
 template<>
-void Serialize(Context &context, const uint32_t& data)
+inline void Serialize(Context &context, const uint32_t& data)
 {
     SerializePrimitive(context, data);
 }
 
 template<>
-void Serialize(Context &context, const uint64_t& data)
+inline void Serialize(Context &context, const uint64_t& data)
 {
     SerializePrimitive(context, data);
 }
 
 template<>
-void Serialize(Context &context, const int8_t& data)
+inline void Serialize(Context &context, const int8_t& data)
 {
     SerializePrimitive(context, data);
 }
 
 template<>
-void Serialize(Context &context, const int16_t& data)
+inline void Serialize(Context &context, const int16_t& data)
 {
     SerializePrimitive(context, data);
 }
 
 template<>
-void Serialize(Context &context, const int32_t& data)
+inline void Serialize(Context &context, const int32_t& data)
 {
     SerializePrimitive(context, data);
 }
 
 template<>
-void Serialize(Context &context, const int64_t& data)
+inline void Serialize(Context &context, const int64_t& data)
 {
     SerializePrimitive(context, data);
 }
 
 template<>
-void Serialize(Context &context, const bool& data)
+inline void Serialize(Context &context, const bool& data)
 {
     SerializePrimitive(context, static_cast<uint8_t>(data));
 }
 
+template<>
+inline void Serialize(Context &context, const char& data)
+{
+    uint8_t byte = *reinterpret_cast<const uint8_t*>(&data);
+    SerializePrimitive(context, byte);
+}
+
+template<>
+inline void Serialize(Context &context, const std::string& data)
+{
+    assert(data.size() <= std::numeric_limits<uint32_t>::max());
+
+    Serialize(context, static_cast<uint32_t>(data.size()));
+    for (char ch : data)
+    {
+        Serialize(context, ch);
+    }
+}
+
 template<typename T>
-bool DeserializePrimitive(Context &context, T& data)
+inline bool DeserializePrimitive(Context &context, T& data)
 {
     size_t spaceRemaining = context.buffer.size() - context.cursor; 
     if (spaceRemaining < sizeof(T))
@@ -112,60 +134,96 @@ bool DeserializePrimitive(Context &context, T& data)
 }
 
 template<>
-bool Deserialize(Context &context, uint8_t& data)
+inline bool Deserialize(Context &context, uint8_t& data)
 {
     return DeserializePrimitive(context, data);
 }
 
 template<>
-bool Deserialize(Context &context, uint16_t& data)
+inline bool Deserialize(Context &context, uint16_t& data)
 {
     return DeserializePrimitive(context, data);
 }
 
 template<>
-bool Deserialize(Context &context, uint32_t& data)
+inline bool Deserialize(Context &context, uint32_t& data)
 {
     return DeserializePrimitive(context, data);
 }
 
 template<>
-bool Deserialize(Context &context, uint64_t& data)
+inline bool Deserialize(Context &context, uint64_t& data)
 {
     return DeserializePrimitive(context, data);
 }
 
 template<>
-bool Deserialize(Context &context, int8_t& data)
+inline bool Deserialize(Context &context, int8_t& data)
 {
     return DeserializePrimitive(context, data);
 }
 
 template<>
-bool Deserialize(Context &context, int16_t& data)
+inline bool Deserialize(Context &context, int16_t& data)
 {
     return DeserializePrimitive(context, data);
 }
 
 template<>
-bool Deserialize(Context &context, int32_t& data)
+inline bool Deserialize(Context &context, int32_t& data)
 {
     return DeserializePrimitive(context, data);
 }
 
 template<>
-bool Deserialize(Context &context, int64_t& data)
+inline bool Deserialize(Context &context, int64_t& data)
 {
     return DeserializePrimitive(context, data);
 }
 
 template<>
-bool Deserialize(Context &context, bool& data)
+inline bool Deserialize(Context &context, bool& data)
 {
     uint8_t tmp;
     bool result = DeserializePrimitive(context, tmp);
     data = static_cast<bool>(tmp);
     return result;
+}
+
+template<>
+inline bool Deserialize(Context &context, char& data)
+{
+    uint8_t byte;
+    if (!DeserializePrimitive(context, byte))
+    {
+        return false;
+    }
+
+    *reinterpret_cast<uint8_t*>(&data) = byte;
+
+    return true;
+}
+
+template<>
+inline bool Deserialize(Context &context, std::string& data)
+{
+    uint32_t length;
+    if (!Deserialize(context, length))
+    {
+        return false;
+    }
+
+    data.resize(length);
+
+    for (uint32_t i = 0; i < length; i++)
+    {
+        if (!Deserialize(context, data[i]))
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 }
---
+++ tests/stl_test.cpp
@@ -0,0 +1,43 @@
+#include <gtest/gtest.h>
+
+#include <cerealizer.h>
+
+using namespace Cerealizer;
+
+TEST(StlTest, HandlesString)
+{
+    std::string a = "xyz", b;
+    Context ctx;
+
+    Serialize(ctx, a);
+    ctx.cursor = 0;
+    EXPECT_TRUE(Deserialize(ctx, b));
+
+    EXPECT_EQ(a, b);
+}
+
+TEST(StlTest, HandlesEmptyString)
+{
+    std::string a = "", b = "asdf";
+    Context ctx;
+
+    Serialize(ctx, a);
+    ctx.cursor = 0;
+    EXPECT_TRUE(Deserialize(ctx, b));
+
+    EXPECT_EQ(a, b);
+}
+
+TEST(StlTest, HandlesInvalidString)
+{
+    std::string b;
+    Context ctx;
+
+    Serialize<uint32_t>(ctx, 10);
+    Serialize<char>(ctx, 'a');
+    Serialize<char>(ctx, 'b');
+    Serialize<char>(ctx, 'c');
+    ctx.cursor = 0;
+
+    EXPECT_FALSE(Deserialize(ctx, b));
+}
